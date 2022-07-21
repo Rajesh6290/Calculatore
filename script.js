7db9394864138c6d71532a08dfd909d95c849291
Rajesh Kumar Behera <98143678+Rajesh6290@users.noreply.github.com>
2022-07-21 23:45:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -24,20 +24,20 @@ function calculate(){
         document.getElementById("remark").innerHTML="<span style='color:#FF0000;'>Fail</span>";
     }
     if(per>=4.0 && per<=5.0){
-        document.getElementById("grade").innerHTML="E";
+        document.getElementById("grade").innerHTML="D";
 
     }
     else if(per>=5.0 && per <= 6.0){
-        document.getElementById("grade").innerHTML="D";
+        document.getElementById("grade").innerHTML="C";
     }
     else if(per >=6.0 && per <= 7.0){
-        document.getElementById("grade").innerHTML="c";
-    }
-    else if(per >=7.0 && per <= 8.5){
         document.getElementById("grade").innerHTML="B";
     }
+    else if(per >=7.0 && per <= 8.5){
+        document.getElementById("grade").innerHTML="A";
+    }
     else if(per>= 8.5 && per <=10.0){
-        document.getElementById("grade").innerHTML="A+";
+        document.getElementById("grade").innerHTML="O";
     }
     else
     {
